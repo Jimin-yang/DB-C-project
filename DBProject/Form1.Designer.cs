2aab5ed287788c1f65b6a2535456495561707a78
Jimin-yang <jimin010303@gmail.com>
2023-11-14 10:31:52 +0900

    회원상세페이지/pt기간관련 임의로db작성부여

---
+++ DBProject/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿
+namespace DBProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtage = new System.Windows.Forms.TextBox();
+            this.txtheight = new System.Windows.Forms.TextBox();
+            this.txtphone = new System.Windows.Forms.TextBox();
+            this.txtptdate = new System.Windows.Forms.TextBox();
+            this.txtptlast = new System.Windows.Forms.TextBox();
+            this.AppendBtn = new System.Windows.Forms.Button();
+            this.UpdateBtn = new System.Windows.Forms.Button();
+            this.DeleteBtn = new System.Windows.Forms.Button();
+            this.DBGrid = new System.Windows.Forms.DataGridView();
+            this.DAOpenBtn = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DBGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(136, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "회원 상세 페이지";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "성명";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "나이";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "키";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(40, 261);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 12);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "전화번호";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(40, 304);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 12);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "PT기간";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(40, 353);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(73, 12);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "PT남은 일수";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(119, 57);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(100, 21);
+            this.txtid.TabIndex = 8;
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(119, 110);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(100, 21);
+            this.txtname.TabIndex = 9;
+            // 
+            // txtage
+            // 
+            this.txtage.Location = new System.Drawing.Point(119, 162);
+            this.txtage.Name = "txtage";
+            this.txtage.Size = new System.Drawing.Size(100, 21);
+            this.txtage.TabIndex = 10;
+            // 
+            // txtheight
+            // 
+            this.txtheight.Location = new System.Drawing.Point(119, 212);
+            this.txtheight.Name = "txtheight";
+            this.txtheight.Size = new System.Drawing.Size(100, 21);
+            this.txtheight.TabIndex = 11;
+            // 
+            // txtphone
+            // 
+            this.txtphone.Location = new System.Drawing.Point(119, 258);
+            this.txtphone.Name = "txtphone";
+            this.txtphone.Size = new System.Drawing.Size(100, 21);
+            this.txtphone.TabIndex = 11;
+            // 
+            // txtptdate
+            // 
+            this.txtptdate.Location = new System.Drawing.Point(119, 301);
+            this.txtptdate.Multiline = true;
+            this.txtptdate.Name = "txtptdate";
+            this.txtptdate.Size = new System.Drawing.Size(163, 21);
+            this.txtptdate.TabIndex = 12;
+            // 
+            // txtptlast
+            // 
+            this.txtptlast.Location = new System.Drawing.Point(119, 350);
+            this.txtptlast.Name = "txtptlast";
+            this.txtptlast.Size = new System.Drawing.Size(100, 21);
+            this.txtptlast.TabIndex = 13;
+            // 
+            // AppendBtn
+            // 
+            this.AppendBtn.Location = new System.Drawing.Point(42, 396);
+            this.AppendBtn.Name = "AppendBtn";
+            this.AppendBtn.Size = new System.Drawing.Size(75, 23);
+            this.AppendBtn.TabIndex = 14;
+            this.AppendBtn.Text = "추가";
+            this.AppendBtn.UseVisualStyleBackColor = true;
+            this.AppendBtn.Click += new System.EventHandler(this.AppendBtn_Click);
+            // 
+            // UpdateBtn
+            // 
+            this.UpdateBtn.Location = new System.Drawing.Point(164, 396);
+            this.UpdateBtn.Name = "UpdateBtn";
+            this.UpdateBtn.Size = new System.Drawing.Size(75, 23);
+            this.UpdateBtn.TabIndex = 15;
+            this.UpdateBtn.Text = "수정";
+            this.UpdateBtn.UseVisualStyleBackColor = true;
+            this.UpdateBtn.Click += new System.EventHandler(this.UpdateBtn_Click);
+            // 
+            // DeleteBtn
+            // 
+            this.DeleteBtn.Location = new System.Drawing.Point(292, 396);
+            this.DeleteBtn.Name = "DeleteBtn";
+            this.DeleteBtn.Size = new System.Drawing.Size(75, 23);
+            this.DeleteBtn.TabIndex = 16;
+            this.DeleteBtn.Text = "삭제";
+            this.DeleteBtn.UseVisualStyleBackColor = true;
+            this.DeleteBtn.Click += new System.EventHandler(this.DeleteBtn_Click);
+            // 
+            // DBGrid
+            // 
+            this.DBGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DBGrid.Location = new System.Drawing.Point(381, 53);
+            this.DBGrid.Name = "DBGrid";
+            this.DBGrid.RowTemplate.Height = 23;
+            this.DBGrid.Size = new System.Drawing.Size(389, 312);
+            this.DBGrid.TabIndex = 17;
+            this.DBGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DBGrid_CellContentClick);
+            // 
+            // DAOpenBtn
+            // 
+            this.DAOpenBtn.Location = new System.Drawing.Point(461, 396);
+            this.DAOpenBtn.Name = "DAOpenBtn";
+            this.DAOpenBtn.Size = new System.Drawing.Size(75, 23);
+            this.DAOpenBtn.TabIndex = 18;
+            this.DAOpenBtn.Text = "조회";
+            this.DAOpenBtn.UseVisualStyleBackColor = true;
+            this.DAOpenBtn.Click += new System.EventHandler(this.DAOpenBtn_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(615, 396);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 19;
+            this.button5.Text = "메인화면";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.DAOpenBtn);
+            this.Controls.Add(this.DBGrid);
+            this.Controls.Add(this.DeleteBtn);
+            this.Controls.Add(this.UpdateBtn);
+            this.Controls.Add(this.AppendBtn);
+            this.Controls.Add(this.txtptlast);
+            this.Controls.Add(this.txtptdate);
+            this.Controls.Add(this.txtphone);
+            this.Controls.Add(this.txtheight);
+            this.Controls.Add(this.txtage);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DBGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtage;
+        private System.Windows.Forms.TextBox txtheight;
+        private System.Windows.Forms.TextBox txtphone;
+        private System.Windows.Forms.TextBox txtptdate;
+        private System.Windows.Forms.TextBox txtptlast;
+        private System.Windows.Forms.Button AppendBtn;
+        private System.Windows.Forms.Button UpdateBtn;
+        private System.Windows.Forms.Button DeleteBtn;
+        private System.Windows.Forms.DataGridView DBGrid;
+        private System.Windows.Forms.Button DAOpenBtn;
+        private System.Windows.Forms.Button button5;
+    }
+}
---
+++ DBProject/Form1.cs
@@ -0,0 +1,182 @@
+﻿using Oracle.DataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+
+namespace DBProject
+{
+    public partial class Form1 : Form
+    {
+        DBClass dbc = new DBClass(); //*****DBClass 객체 생성
+        private void ClearTextBoxes()
+        {
+            txtid.Clear();
+            txtname.Clear();
+            txtage.Clear();
+            txtheight.Clear();
+            txtphone.Clear();
+            txtptdate.Clear();
+            txtptlast.Clear();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            dbc.DB_ObjCreate(); // *****
+            dbc.DB_Open(); // *****
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DAOpenBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dbc.DS.Clear();
+                dbc.DBAdapter.Fill(dbc.DS, "customer");
+                DBGrid.DataSource = dbc.DS.Tables["customer"].DefaultView;
+            }
+            catch (DataException DE)
+            {
+                MessageBox.Show(DE.Message);
+            }
+            catch (Exception DE)
+            {
+                MessageBox.Show(DE.Message);
+            }
+        }
+
+        private void AppendBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MessageBox.Show("텍스트 상자에 모든 데이터 입력하셨으면 추가합니다!");
+                //DS.Clear();
+                //DBAdapter.Fill(DS, "Phone");
+                dbc.CustomerTable = dbc.DS.Tables["Customer"]; //*
+                DataRow newRow = dbc.CustomerTable.NewRow();
+                newRow["id"] = Convert.ToInt32(txtid.Text);
+                newRow["cName"] = txtname.Text;
+                newRow["cage"] = txtage.Text;
+                newRow["cheight"] = txtheight.Text;
+                newRow["phone"] = txtphone.Text;
+                newRow["cptdate"] = txtptdate.Text;
+                newRow["cptlastdate"] = txtptlast.Text;
+                dbc.CustomerTable.Rows.Add(newRow);
+                dbc.DBAdapter.Update(dbc.DS, "Customer");
+                dbc.DS.AcceptChanges();
+                ClearTextBoxes();
+                DBGrid.DataSource = dbc.DS.Tables["Customer"].DefaultView;
+            }
+            catch (DataException DE)
+            {
+                MessageBox.Show(DE.Message);
+            }
+            catch (Exception DE)
+            {
+                MessageBox.Show(DE.Message);
+            }
+        }
+
+        private void UpdateBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //DS.Clear();
+                //DBAdapter.Fill(DS, "Phone");
+                dbc.CustomerTable = dbc.DS.Tables["Customer"];
+                DataColumn[] PrimaryKey = new DataColumn[1];
+                PrimaryKey[0] = dbc.CustomerTable.Columns["id"];
+                dbc.CustomerTable.PrimaryKey = PrimaryKey;
+                DataRow currRow = dbc.CustomerTable.Rows.Find(dbc.SelectedRowIndex);
+                currRow.BeginEdit();
+                currRow["id"] = txtid.Text;
+                currRow["cName"] = txtname.Text;
+                currRow["cage"] = txtage.Text;
+                currRow["cheight"] = txtheight.Text;
+                currRow["phone"] = txtphone.Text;
+                currRow["cptdate"] = txtptdate.Text;
+                currRow["cptlastdate"] = txtptlast.Text;
+                currRow.EndEdit();
+                DataSet UpdatedSet = dbc.DS.GetChanges(DataRowState.Modified);
+                if (UpdatedSet.HasErrors)
+                { MessageBox.Show("변경된 데이터에 문제가 있습니다."); }
+                else
+                { dbc.DBAdapter.Update(UpdatedSet, "Customer"); dbc.DS.AcceptChanges(); }
+                DBGrid.DataSource = dbc.DS.Tables["Customer"].DefaultView;
+            }
+            catch (DataException DE)
+            { MessageBox.Show(DE.Message); }
+            catch (Exception DE)
+            { MessageBox.Show(DE.Message); }
+        }
+
+        private void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //DS.Clear();
+                //DBAdapter.Fill(DS, "Phone");
+                dbc.CustomerTable = dbc.DS.Tables["Phone"];
+                DataColumn[] PrimaryKey = new DataColumn[1];
+                PrimaryKey[0] = dbc.CustomerTable.Columns["id"];
+                dbc.CustomerTable.PrimaryKey = PrimaryKey;
+                DataRow currRow = dbc.CustomerTable.Rows.Find(dbc.SelectedRowIndex);
+                currRow.Delete();
+                dbc.DBAdapter.Update(dbc.DS.GetChanges(DataRowState.Deleted), "Customer");
+                DBGrid.DataSource = dbc.DS.Tables["Customer"].DefaultView;
+            }
+            catch (DataException DE)
+            {
+                MessageBox.Show(DE.Message);
+            }
+            catch (Exception DE)
+            {
+                MessageBox.Show(DE.Message);
+            }
+        }
+
+        private void DBGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                //DataSet DS = new DataSet();
+                //DBAdapter.Fill(DS, "Phone");
+                DataTable CustomerTable = dbc.DS.Tables["Customer"];
+                if (e.RowIndex < 0)
+                {
+                    return;
+                }
+                else if (e.RowIndex > CustomerTable.Rows.Count - 1)
+                {
+                    MessageBox.Show("해당하는 데이터가 존재하지 않습니다.");
+                    return;
+                }
+                DataRow currRow = CustomerTable.Rows[e.RowIndex];
+                txtid.Text = currRow["id"].ToString();
+                txtname.Text = currRow["cname"].ToString();
+                txtage.Text = currRow["cage"].ToString();
+                txtheight.Text = currRow["cheight"].ToString();
+                txtphone.Text = currRow["phone"].ToString();
+                txtptdate.Text = currRow["cptdate"].ToString();
+                txtptlast.Text = currRow["cptlastdate"].ToString();
+                dbc.SelectedRowIndex = Convert.ToInt32(currRow["id"]);
+            }
+            catch (DataException DE)
+            { MessageBox.Show(DE.Message); }
+            catch (Exception DE)
+            { MessageBox.Show(DE.Message); }
+        }
+
+    }
+}
+
